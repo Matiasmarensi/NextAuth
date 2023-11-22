b5fbd560ccde2b982e3c77841e68d42bed642c4b
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-11-21 22:01:41 -0300

    auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,41 @@
 import nextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { conectDB } from "@/libs/mogodb";
+import User from "@/models/users";
+import bcryptjs from "bcryptjs";
 
 const handler = nextAuth({
   providers: [
     CredentialsProvider({
       name: "credentials",
       credentials: {
-        email: { label: "email", type: "email" },
+        email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        const user = { id: "1", fullname: "jhon", email: "jhon@gmail.com", password: "password" };
-        return user;
+        await conectDB();
+        console.log("credential", credentials);
+        const userFound = await User.findOne({ email: credentials?.email });
+
+        console.log("user", userFound);
+
+        return userFound;
       },
     }),
   ],
+  callbacks: {
+    jwt({ account, token, user, profile, session }) {
+      token.hello = "HOLAAAAAA";
+      return token;
+    },
+    session({ session, token }) {
+      session.user = token.user as any;
+
+      console.log({ session, token });
+
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
